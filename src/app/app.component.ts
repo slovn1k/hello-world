4c917e35e7bf73d8be25c37316f150f9aa1d5036
Alexandr Turcanu <turcanualexandru1992@gmail.com>
2018-07-31 18:54:42 +0300

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  // Changed the title of the app
+  title = 'Angular APP';
 }
